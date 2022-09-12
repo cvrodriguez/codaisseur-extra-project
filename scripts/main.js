9d1e03d1df0527972cbfeedb2874ede1e5ff38b9
cvrodriguez <cinvir15@gmail.com>
2022-09-12 16:39:21 +0200

    makeover first step

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
-<script src="scripts/main.js" type="text/javascript"></script>
+function updateCounters() {
+    // declare a variable that contains the "Total" counter element
+    // declare a variable that contains the number of todos, by counting how many elements have a specific classname / attribute
+    // update the HTML inside the counter element with the number of todos
+  }
+  
+  updateCounters();
+  const listItems = document.querySelectorAll("li");
+
+console.log(`We have ${listItems.length} list items on this page`);
