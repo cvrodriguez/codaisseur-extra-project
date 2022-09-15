db97b5526358e336e1068f9d50b265f626495d77
cvrodriguez <cinvir15@gmail.com>
2022-09-15 14:03:07 +0200

    add updateCounters funtion

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,27 @@
 function updateCounters() {
-    // declare a variable that contains the "Total" counter element
-    const totalList = 0;
-    // declare a variable that contains the number of todos, by counting how many elements have a specific classname / attribute
-    // update the HTML inside the counter element with the number of todos
-  }
-  
-  updateCounters();
+  // declare a variable that contains the "Total" counter element
+  const totalList = document.querySelector("#total");
+  console.log(totalList)
+  // declare a variable that contains the number of todos, by counting how many elements have a specific classname / attribute
   const listItems = document.querySelectorAll("li");
+  console.log(listItems)
+  // update the HTML inside the counter element with the number of todos
+  totalList.innerText = listItems.length
+ 
+  
+  // TODOS NOT DONE
+  const notDone = document.querySelectorAll(".doNot-checked")
+  const stillTodo = document.querySelectorAll("#toDo")
+  stillTodo.innerText = notDone.length
+  console.log(stillTodo)
+// TODOS DONE
+  const done = document.querySelectorAll(".do-checked")
+  const toDoHTML = document.querySelector("#done")
+  toDoHTML.innerText = done.length
+  console.log(toDoHTML)
 
-console.log(`We have ${listItems.length} list items on this page`);
+}
+
+updateCounters();
+
+
