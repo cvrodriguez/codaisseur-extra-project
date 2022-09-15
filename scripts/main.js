e553eff9f1400e6b283904bcb4a3d094fee4351a
cvrodriguez <cinvir15@gmail.com>
2022-09-15 10:49:56 +0200

    makeover again the html

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 function updateCounters() {
     // declare a variable that contains the "Total" counter element
+    const totalList = 0;
     // declare a variable that contains the number of todos, by counting how many elements have a specific classname / attribute
     // update the HTML inside the counter element with the number of todos
   }
